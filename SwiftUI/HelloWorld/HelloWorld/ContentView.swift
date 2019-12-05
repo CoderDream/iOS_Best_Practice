d1dd61a640adf2ce96c400545901ea0e477e91ec
coderdream <coderdream@gmail.com>
2019-12-04 16:32:26 -0800

    add SwiftUI HelloWorld 201915050832

---
+++ SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by coderdream on 2019/12/3.
+//  Copyright © 2019 coderdream. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
